5232f85c5b5d769239ce5e46b9364deb361edae8
hacker-pyrat <parent.j.f@gmail.com>
2016-06-02 12:38:35 +0300

    Added stuffs

--- trainementss.py
+++ trainementss.py
@@ -1,16 +1,34 @@
 #! /usr/bin/env python
 
-from random import choice
+from random import sample, choice
 
 import click
 
-exercises = \
-    ['Crunches'] * 2 + \
-    ['Squat'] * 3 + \
-    ['Lounge'] * 3 + \
-    ['Vsit'] * 3 + \
-    ['Padmasana'] * 3 + \
-    ['Sirshasana'] * 3
+mobilities = [
+    "Neck",
+    "Shoulder",
+    "Elbow, Wrist, Hand & Finger",
+    "Spine & Lower Back",
+    "Hip Mobility",
+    "Toe, Foot, Ankle & Knee",
+    "Spinal Rocking"
+]
+
+exercises = [
+    "Back Extension",
+    "Ball Triceps Extension",
+    "Hamstring Curl",
+    "3min walking on the toe",
+    "3min walking on the heel",
+    "3min sitting on the heel",
+    "Redressement assis",
+    "coup de pied lent",
+    "Plank",
+    "Squat",
+    "Lounge",
+    "L-sit",
+    "Pistol squat",
+]
 
 quotes = [
     "Sweat is Fat Crying",
@@ -20,6 +38,8 @@ quotes = [
     "Strive for progress, not perfection.",
     "You want me to do something... tell me I can't do it. -Maya Angelou",
     "Energy and persistence conquer all things. -Benjamin Franklin",
+    "Use only that which works, and take it from any place you can find it. -Bruce Lee",
+    "Things do NOT happen. Things are made to happen -John F. Kennedy",
 ]
 
 @click.group()
@@ -29,10 +49,22 @@ def cli():
 @click.command()
 @click.argument('number', default=1, type=int)
 def random(number):
+    #QUOTE
     print '-'*80
     print '[*]', choice(quotes)
     print '-'*80
-    for _ in xrange(number):
-        print '[*]', choice(exercises)
+    #WIM-HOF BREATHING
+    print '[*] Breath Motherfucker!'
+    print '-'*80
+    #BODYWEIGHT TRAINING
+    print '[*] Pompe'
+    samples = sample(exercises, number)
+    for s in samples:
+        print '[*]', s
+
+    #MOBILITY
+    print '-'*80
+    print '[*]', choice(mobilities)
+    print '-'*80
 
 cli.add_command(random)
